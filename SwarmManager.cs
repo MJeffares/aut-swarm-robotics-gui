3df7a35cdcf44297e850c37332d0e4fdd8c6f2f8
Mansel Jeffares <Manselj@gmail.com>
2017-09-19 18:49:17 +1200

    X/Y fix

--- SwarmManager.cs
+++ SwarmManager.cs
@@ -1,4 +1,28 @@
-﻿using System.Linq;
+﻿/**********************************************************************************************************************************************
+*	File: SwarmManager.cs
+*
+*	Developed By: Mansel Jeffares
+*	First Build: 19 October 2017
+*	Current Build: 19 October 2017
+*
+*	Description :
+*		Developed for a final year university project at Auckland University of Technology (AUT), New Zealand
+*		
+*	Useage :
+*		
+*
+*	Limitations :
+*		Build for x64, .NET 4.5.2
+*   
+*		Naming Conventions:
+*			Variables, camelCase, start lower case, subsequent words also upper case, if another object goes by the same name, then also with an underscore
+*			Methods, PascalCase, start upper case, subsequent words also upper case
+*			Constants, all upper case, unscores for seperation
+* 
+**********************************************************************************************************************************************/
+
+
+using System.Linq;
 using System;
 using System.Collections.Generic;
 using System.Windows;
@@ -59,12 +83,12 @@ namespace SwarmRoboticsGUI
 
                 num = (UInt16)R.Location.X;
 
-                data[1] = (byte)(num >> 0xFF);
+                data[1] = (byte)(num >> 0x8);
                 data[2] = (byte)(num);
 
                 num = (UInt16)R.Location.Y;
 
-                data[3] = (byte)(num >> 0xFF);
+                data[3] = (byte)(num >> 0x8);
                 data[4] = (byte)(num);
 
 
--- TowerControl.cs
+++ TowerControl.cs
@@ -1,1 +1,22 @@
-﻿
+﻿/**********************************************************************************************************************************************
+*	File: SwarmManager.cs
+*
+*	Developed By: Mansel Jeffares
+*	First Build: 19 October 2017
+*	Current Build: 19 October 2017
+*
+*	Description :
+*		Developed for a final year university project at Auckland University of Technology (AUT), New Zealand
+*		
+*	Useage :
+*		
+*
+*	Limitations :
+*		Build for x64, .NET 4.5.2
+*   
+*		Naming Conventions:
+*			Variables, camelCase, start lower case, subsequent words also upper case, if another object goes by the same name, then also with an underscore
+*			Methods, PascalCase, start upper case, subsequent words also upper case
+*			Constants, all upper case, unscores for seperation
+* 
+**********************************************************************************************************************************************/
